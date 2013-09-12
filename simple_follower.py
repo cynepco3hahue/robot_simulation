7fe291f7988fd7a43d616c66aa672e1ff12e082d
omrikere <timtam30@walla.com>
2013-09-12 13:31:31 +0200

    Update simple_follower.py

--- simple_follower.py
+++ simple_follower.py
@@ -1,20 +1,29 @@
 import robo3pi as s
 import time
 
+################################################################
+# main program - unless the robots are told differently, they should follow elliptic tracks of a size known in advance.
+###############################################################
 
 # This function makes the robot with the given id move along a track with a
 #  ~25cm part of a straight line, and a circular part with radius ~10cm.
 #  All time and speed calculations are done empirically.
 
+global is_on_the_curve
+
 def follow_the_line(id):
     def make_n_turns(n):
         for i in range(n):
+			if i == 0:
+				is_on_the_curve[id] = 1 
             s.move(id, -20, 20)
             time.sleep(0.35)
             s.move(id, 20, 20)
-            time.sleep(0.5)
+            time.sleep(0.8)
+			if i == n - 1:
+				is_on_the_curve[id] = 0
+				
     # to be used later on
-
     def fix_location(id):
         pos = s.readLinePosition(id)
         time.sleep(0.2)
@@ -40,14 +49,15 @@ def follow_the_line(id):
                 s.move(id, 20, 20)
                 time.sleep(0.3)
 
-    s.init("/dev/ttyUSB0")
+    s.init('com4')
+	is_on_the_curve = [0*i for i in range(5)] # at first, all robots are on the straight line
     #s.readCalibratedSensors(id, 1)
     #time.sleep(0.35)
     start = time.time()
     while time.time() - start < 60: # for one minute
         s.move(id, 20, 20)
-        time.sleep(3.2)
-        make_n_turns(3)
+        time.sleep(5)
+		make_n_turns(id + 6)
         s.move(id, -20, 20)
         time.sleep(0.7)
         s.move(id, 20, 20)
@@ -58,4 +68,19 @@ def follow_the_line(id):
     s.stop(id)
 
 
-follow_the_line('5')
+def main():
+# The first robot to start moving is the leader of the flock (assuming the identity permutation),
+# then the ones which are behind him, and finally those behind them. After one second all robots will be moving.
+# Each of the robots is given different timings and set of orders according to its position with respect of the rest of the flock.
+	follow_the_line(3)
+	time.sleep(0.2)
+	follow_the_line(2)
+	time.sleep(0.2)
+	follow_the_line(4)
+	time.sleep(0.2)
+	follow_the_line(1)
+	time.sleep(0.2)
+	follow_the_line(5)
+	time.sleep(0.2)
+	
+main()
